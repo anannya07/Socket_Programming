befa4e00aaf5083f4044b0efc3fe437bd852f4b0
Anannya Guchait <131895159+anannya07@users.noreply.github.com>
2025-03-27 23:58:48 +0530

    Update crcclient.c

--- crcclient.c
+++ crcclient.c
@@ -2,33 +2,33 @@
 #include<string.h>
 #include<arpa/inet.h>
 #include<unistd.h>
+#include<math.h>
 int main(){
-int sd,cadl,i,k,len;
-char rem[50];
+int sd,cadl;
 struct sockaddr_in sad,cad;
-char dataword[50],generator[50],fcrc[50],crc[50];
+
+char dataword[50],generator[50],crc[50];
+int i,k,len;
+
 sd=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 sad.sin_family=AF_INET;
-sad.sin_port=htons(5779);
+sad.sin_port=htons(9998);
 sad.sin_addr.s_addr=inet_addr("127.0.0.1");
 connect(sd, (struct sockaddr *)&sad, sizeof(sad));
-recv(sd, dataword, sizeof(dataword), 0);
-recv(sd, generator, sizeof(generator),0);
-recv(sd,crc,sizeof(crc),0);
-recv(sd,fcrc,sizeof(fcrc),0);
-
-printf("\nDataword  recv from sender is %s \n",dataword);
-printf("\nGenerator  recv from sender is %s \n",generator);
-fflush(stdin);
+printf("Enter the dataword:");
+scanf("%s",dataword);
+strcpy(crc,dataword);
+printf("Enter the generator or in polynomial form:");
+scanf("%s",generator);
 int dsize=strlen(dataword);
 int gsize=strlen(generator);
-char ch;
-printf("Do you want to insert any remainder:?y/n\n");
-scanf("%c",&ch);
-if(ch=='y'){
-    printf("Enter the remainder of generator-1 bits:\n");
-    scanf("%s",rem);
-    strcat(dataword,rem);
+// first add 0's for generator-1
+    for( i=0;i<gsize-1;++i){
+        dataword[dsize+i]='0';
+    }
+    dataword[dsize+gsize-1]='\0';
+   // printf("%s\n",dataword);
+    // now perfprm the modulo 2 division
     do{
         for(i=0;i<gsize;++i){
             dataword[i]=(dataword[i]==generator[i])?'0':'1';
@@ -50,24 +50,18 @@ if(ch=='y'){
         len=dsize+gsize-1;
     }while(len>gsize-1);
     dataword[len]='\0';
-    //printf("%s",dataword);
-    int error_detected = 0;
-        for (i = 0; i < strlen(dataword); ++i) {
-            //printf("%c",dataword[i]);
-            if (dataword[i] != '0') {
-                error_detected = 1;
-                break;
-            }
-        }
 
-        if (error_detected) {
-            printf("Wrong CRC remainder!!\n");
-            printf("Correct CRC received from sender: %s\n", fcrc);
-        } else {
-            printf("No errors detected in received data.\n");
-        }
-    } else {
-        printf("Ultimate CRC received from sender: %s\n", crc);
-    }
+    fflush(stdin);
+
+printf("The remainder of  crc is %s\n",dataword);
+strcat(crc,dataword);
+printf("The codeword is %s\n",crc);
+send(sd, crc, sizeof(crc), 0);
+/*recv(cd, str, sizeof(str), 0);
+printf("\nRecieved data is : %s\n", str);*/
 close(sd);
 }
+/*gcc checksumclient.c -o cclient -lm
+
+./cclient
+*/
